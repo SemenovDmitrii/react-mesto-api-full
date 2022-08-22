d47250b22579976083f277e2545490f7e30ee5ad
SemenovDmitrii <semenov.dmitrii.s.d.v@yandex.ru>
2022-08-22 10:16:41 +0300

    fix app

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -9,7 +9,7 @@ module.exports = (req, res, next) => {
   }
   let payload;
   try {
-    payload = jwt.verify(token, NODE_ENV !== 'production' ? JWT_SECRET : 'dev-secret');
+    payload = jwt.verify(token, NODE_ENV !== 'production' ? JWT_SECRET : 'some-secret-key');
   } catch (err) {
     throw new UnauthorizedError('Ошибка токена. Необходима повторная авторизация.');
   }
