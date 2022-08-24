07dc453099eb7be69fa8a33f481025830c97eb67
SemenovDmitrii <semenov.dmitrii.s.d.v@yandex.ru>
2022-08-24 19:25:40 +0300

    fix app

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,10 @@
 require('dotenv').config();
 const express = require('express');
-const cors = require('cors');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const { errors } = require('celebrate');
-const auth = require('./middlewares/auth');
+const { auth, cors } = require('./middlewares/auth');
 const routerUsers = require('./routes/users');
 const routerCards = require('./routes/cards');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
@@ -21,31 +20,6 @@ const {
 const app = express();
 
 app.use(cors);
-
-const allowedCors = [
-  'http://sdv.nomoredomains.sbs',
-  'https://sdv.nomoredomains.sbs',
-  'http://api.sdv.nomoredomains.sbs',
-  'https://api.sdv.nomoredomains.sbs',
-  'http://localhost:3000',
-  'https://localhost:3000',
-];
-
-app.use((req, res, next) => {
-  const { origin } = req.headers;
-  const { method } = req;
-  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
-  const requestHeaders = req.headers['access-control-request-headers'];
-  if (allowedCors.includes(origin)) {
-    res.header('Access-Control-Allow-Origin', origin);
-  }
-  if (method === 'OPTIONS') {
-    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
-    res.header('Access-Control-Allow-Headers', requestHeaders);
-    return res.end();
-  }
-  next();
-});
 
 mongoose.connect((NODE_ENV === 'production' ? MONGODB : 'mongodb://localhost:27017/mestodb'), {
   useUnifiedTopology: true,
